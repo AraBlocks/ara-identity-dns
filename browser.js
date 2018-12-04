76f7bcbd27516b4bf71503f75dd33b8686b88d43
Madeline Cameron <github@maddie.today>
2018-12-04 16:14:30 -0500

    fix(browser.js): Fix CORS issue

--- browser.js
+++ browser.js
@@ -1,36 +1,21 @@
-const DNSHTTP = require('dns-over-http')
+function resolve(address) {
+  return window.fetch(`https://dns.google.com/resolve?name=${address}&type=TXT`, { method: "GET" })
+    .then(async (response) => {
+      const body = await response.json()
+      const { Answer: answers } = body
 
-const {
-  Questions,
-  onanswer
-} = require('./lib/utils')
+      if (Array.isArray(answers)) {
+        const dids = []
+        for (const answer of answers) {
+          dids.push(answer.data.replace(/\"/g, ''))
+        }
 
-function resolve(address, opts, cb) {
-  if ('function' === typeof opts) {
-    cb = opts
-    opts = {}
-  }
-
-  const _opts = Object.assign({ url: 'https://dns.google.com/experimental' }, opts)
-
-  const questions = Questions(address)
-
-  DNSHTTP.query(_opts, questions, (err, res) => {
-    if (err) {
-      return cb(err)
-    }
-
-    const { answers } = res
-    if (Array.isArray(answers)) {
-      const dids = []
-      for (const answer of answers) {
-        dids.push(onanswer(answer))
+        return dids
+      } else {
+        return []
       }
-
-      return cb(null, dids)
-    }
-    return cb(null, [])
-  })
+    })
+    .catch(console.error)
 }
 
 module.exports = {
