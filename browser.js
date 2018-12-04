848ee0e63cebe11d127ec6230f78a951c1bf3fc9
Madeline Cameron <github@maddie.today>
2018-12-04 13:30:47 -0500

    feat(): Support browsers, support multiple answers

---
+++ browser.js
@@ -0,0 +1,39 @@
+const DNSHTTP = require('dns-over-http')
+
+const {
+  Questions,
+  onanswer
+} = require('./lib/utils')
+
+function resolve(address, opts, cb) {
+  if (typeof opts == 'function') {
+    cb = opts
+    opts = {}
+  }
+
+  const _opts = Object.assign({ url: 'https://dns.google.com/experimental' }, opts)
+
+  const questions = Questions(address)
+
+  DNSHTTP.query(_opts, questions, (err, res) => {
+    if (err) {
+      return cb(err)
+    }
+
+    const { answers } = res
+    if (Array.isArray(answers)) {
+      const dids = []
+      for (const answer of answers) {
+        dids.push(onanswer(answer))
+      }
+
+      return cb(null, dids)
+    } else {
+      return cb(null, [])
+    }
+  })
+}
+
+module.exports = {
+  resolve
+}
